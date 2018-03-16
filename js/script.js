e8ace77f5928126806056fba1dc7e32fcfaa0169
kollhran <chombart.felix@outlook.com>
2018-03-16 13:58:31 +0100

    fonction ouverture menu et fermeture pour mobile corriger

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-	$(".fermer").click(function(){
+	$(document).on("click",".fermer",function(){
 		$("#header").addClass("vueMenu");
 		$("#menu").addClass("ouvert");
 		$("#header").removeClass("invisible");
 		$("#menu").removeClass("fermer");	
 	});
-	$(".ouvert").click(function(){
+	$(document).on("click",".ouvert",function(){
+		console.log("coucou");
 		$("#header").addClass("invisible");
 		$("#menu").addClass("fermer");
 		$("#header").removeClass("vueMenu");
